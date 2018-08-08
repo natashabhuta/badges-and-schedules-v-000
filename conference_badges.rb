f8a93d7e0ec23aa820ef549e0de12f2e5cda30e8
natashabhuta <natasha.bhuta@gmail.com>
2018-08-08 20:54:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(speakers)
+  badge_messages = []  
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
+end
+
+def assign_rooms(speakers)
+  room_number = 1
+  room_messages = []
+  speakers.each do |speaker|
+    room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    room_number += 1
+  end
+  room_messages
+end 
+
+def
